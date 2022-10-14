88ba1e3b022547b0f6a6b57efa4c3907d0f8ebda
Jack9025 <91426321+Jack9025@users.noreply.github.com>
2022-10-14 23:03:57 +0100

    Updated short process argument for parser

--- distributed_algorithms/cli.py
+++ distributed_algorithms/cli.py
@@ -28,13 +28,13 @@ def cli():
     # Tree wave algorithm
     parser_tree = subparsers.add_parser('tree', help='runs the tree wave algorithm')
     parser_tree.set_defaults(func=tree_algorithm)
-    parser_tree.add_argument('-n', '--processes', help='number of processes',
+    parser_tree.add_argument('-p', '--processes', help='number of processes',
                              type=int, default=8, choices=range(2, 65))
 
     # Echo wave algorithm
     parser_echo = subparsers.add_parser('echo', help='runs the echo wave algorithm')
     parser_echo.set_defaults(func=echo_algorithm)
-    parser_echo.add_argument('-n', '--processes', help='number of processes',
+    parser_echo.add_argument('-p', '--processes', help='number of processes',
                              type=int, default=8, choices=range(2, 65))
     parser_echo.add_argument('-e', '--executions', help='number of executions',
                              type=int, default=1, choices=range(1, 10))
@@ -44,13 +44,13 @@ def cli():
     # Tree election algorithm
     parser_tree_election = subparsers.add_parser('tree_election', help='runs the tree election algorithm')
     parser_tree_election.set_defaults(func=tree_election_algorithm)
-    parser_tree_election.add_argument('-n', '--processes', help='number of processes',
+    parser_tree_election.add_argument('-p', '--processes', help='number of processes',
                                       type=int, default=8, choices=range(2, 65))
 
     # Echo election algorithm
     parser_echo_election = subparsers.add_parser('echo_election', help='runs the echo election algorithm')
     parser_echo_election.set_defaults(func=echo_election_algorithm)
-    parser_echo_election.add_argument('-n', '--processes', help='number of processes',
+    parser_echo_election.add_argument('-p', '--processes', help='number of processes',
                                       type=int, default=8, choices=range(2, 65))
     parser_echo_election.add_argument('-i', '--initiators', help='number of initiators',
                                       type=int, default=1, choices=range(1, 65))
